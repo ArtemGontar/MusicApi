c4987e691c54ddcb58436eb6a64aa03cd236dcdd
art.hontar <art.hontar@andersenlab.com>
2019-06-13 15:01:34 +0300

    add unit test proj

---
+++ Tests/Music.Tests.BussinessLogicServicesTests/ArtistsServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Music.Tests.BussinessLogicServicesTests
+{
+    [TestClass]
+    public class ArtistsServiceTests
+    {
+        [TestMethod]
+        public async Task GetAllArtistsAsync_ShouldReturnNotNull()
+        {
+            //Arrange
+
+            //Act
+            //Assert
+            Assert.IsNull(null);
+        }
+    }
+}
---
+++ Tests/Music.Tests.BussinessLogicServicesTests/SongsServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Music.Tests.BussinessLogicServicesTests
+{
+    [TestClass]
+    public class SongsServiceTests
+    {
+        [TestMethod]
+        public async Task GetAllSongsAsync_ShouldReturnNotNull()
+        {
+            //Arrange
+
+            //Act
+            //Assert
+            Assert.IsNull(null);
+        }
+    }
+}
