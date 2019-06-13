26bd9aa2a8abf23b218a0b63c6a36881b2a5072a
art.hontar <art.hontar@andersenlab.com>
2019-06-13 10:22:59 +0300

    add thread enricher

---
+++ Music.WebAPI/Infrastructure/ThreadIdEnricher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Music.WebAPI.Infrastructure
+{
+    /// <summary>
+    /// Enricher class for serilog.
+    /// </summary>
+    public class ThreadIdEnricher : ILogEventEnricher
+    {
+        /// <summary>
+        ///  Add threadId to logs
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// <param name="propertyFactory"></param>
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+}
